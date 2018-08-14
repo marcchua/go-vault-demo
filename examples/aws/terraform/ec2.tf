e47a00329d98f22acfd990128c3390e892c11b86
Lance Larsen <lance@hashicorp.com>
2018-08-13 17:06:58 -0700

    aws hardening

--- examples/aws/terraform/ec2.tf
+++ examples/aws/terraform/ec2.tf
@@ -13,14 +13,21 @@ resource "aws_key_pair" "go" {
   public_key = "${tls_private_key.go.public_key_openssh}"
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "go_allow_all"
-  description = "Allow all inbound traffic"
+resource "aws_security_group" "go_auth_demo" {
+  name        = "go_auth_demo"
+  description = "Allow inbound for ssh and go"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -34,13 +41,13 @@ resource "aws_security_group" "allow_all" {
 
 
 resource "aws_instance" "go-iam" {
-  count = 1
+  count = "${var.aws_instances}"
   ami           = "${data.aws_ami.go.id}"
   instance_type = "t2.micro"
   iam_instance_profile = "${aws_iam_instance_profile.go.name}"
   associate_public_ip_address = true
   key_name = "${aws_key_pair.go.key_name}"
-  security_groups = ["${aws_security_group.allow_all.name}"]
+  security_groups = ["${aws_security_group.go_auth_demo.name}"]
   tags {
     env = "${var.aws_env}"
   }
@@ -69,13 +76,13 @@ SCRIPT
 }
 
 resource "aws_instance" "go-ec2" {
-  count = 1
+  count = "${var.aws_instances}"
   ami           = "${data.aws_ami.go.id}"
   instance_type = "t2.micro"
   iam_instance_profile = "${aws_iam_instance_profile.go.name}"
   associate_public_ip_address = true
   key_name = "${aws_key_pair.go.key_name}"
-  security_groups = ["${aws_security_group.allow_all.name}"]
+  security_groups = ["${aws_security_group.go_auth_demo.name}"]
 
   tags {
     env = "${var.aws_env}"
