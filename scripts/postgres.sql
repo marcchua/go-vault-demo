dab4c0964366bc0bd85b17733e927416da7b99dc
Lance Larsen <lance@hashicorp.com>
2018-08-20 15:19:52 -0700

    Update postgres.sql

--- scripts/postgres.sql
+++ scripts/postgres.sql
@@ -1,6 +1,6 @@
 CREATE TABLE orders (
     id bigserial primary key,
-    customer_name varchar(60) NOT NULL,
+    customer_name varchar(120) NOT NULL,
     product_name varchar(20) NOT NULL,
     order_date timestamp NOT NULL
 );
