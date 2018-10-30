036c257414d3fbc90dec8097ac93c969e43fa0a7
Lance Larsen <lance@hashicorp.com>
2018-10-30 11:40:50 -0700

    Update vault.go

--- client/vault.go
+++ client/vault.go
@@ -141,6 +141,7 @@ func (v *Vault) Initialize() error {
 			creds := stscreds.NewCredentials(stsSession, v.Credential)
 			svc = sts.New(stsSession, &aws.Config{Credentials: creds})
 		} else {
+			log.Printf("SA: Using IAM instance profile")
 			svc = sts.New(stsSession)
 		}
 
